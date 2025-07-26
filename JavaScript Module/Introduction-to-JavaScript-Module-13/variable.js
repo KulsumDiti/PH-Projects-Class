4bf283f5a773381b78aee59698c816984cf16b1f
KulsumDiti <kulsumditi3@gmail.com>
2025-07-26 22:01:25 +0600

    javascript file Added

---
+++ JavaScript Module/Introduction-to-JavaScript-Module-13/variable.js
@@ -0,0 +1,27 @@
+/**
+ * VARIABLE ==> vary + able
+ * 5 things to declare a variable :
+ * 1. keyword like var , let , const
+ * 2. variable name 
+ * 3. equal sign (=)
+ * 4. man 
+ * 5. Semi-colon (;)
+*/
+var weight = 88;
+var price = 28;
+var year = 1997;
+var money = 2025;
+money = 5000;
+
+console.log(money);
+console.log(price);
+
+
+var age = 28;
+age = 98;
+
+console.log(age);
+
+
+
+
