2cebd51a169bd22faa5b7f45bd3e46daec3f7a44
KulsumDiti <kulsumditi3@gmail.com>
2025-07-30 10:26:38 +0600

    js loop file Added

---
+++ JavaScript Module/JS-conditionals-tasks/1-free-drinks.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+ const burgerPrice = 550;
+ if(burgerPrice>= 500){
+    console.log('Free Coke');
+ }
+ else{
+    console.log('give 30tk');
+ }
