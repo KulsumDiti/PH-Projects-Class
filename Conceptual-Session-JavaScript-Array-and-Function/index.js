966cf21fc3b56459f277f6ad0e821a182c8fbeee
KulsumDiti <kulsumditi3@gmail.com>
2025-08-12 21:29:17 +0600

    Js Problem solving part-1

--- Conceptual-Session-JavaScript-Array-and-Function/index.js
+++ Conceptual-Session-JavaScript-Array-and-Function/index.js
@@ -152,6 +152,10 @@
 
 // for (const person of persons) {
 // 	console.log(
+// "name: " + person.name,
+//   "age: " + person.age,
+//   "Student Status: " + person.isStudent);
+// OR  Tamplate - String / (Backtik)use kore:
 // 		`name: ${person.name}, Age: ${person.age}, Student Status: ${person.isStudent}`,
 // 	)
 // }
@@ -169,7 +173,7 @@
 
 // Function syntax
 function greeting() {
-	console.log('Good Evening!')
+  console.log("Good Evening!");
 }
 // greeting() // function call
 
@@ -180,154 +184,171 @@ function greeting() {
  * anonymous functional
  *  IIFE: Immediately Invoked Function Expression, is a JavaScript design pattern where a function is defined and executed immediately after its creation.
  */
-// * normal function
-// function add(val1, val2) {
-// 	console.log('result', val1 + val2)
-// }
-// add(10, 20)
-// function add(val1, val2) {
-// 	const result = val1 + val2
-// 	return result
-// }
+/* normal function
+function add(val1, val2) {
+	console.log('result', val1 + val2)
+}
+add(10, 20)*/
 
-// const addValue = add(20, 30) * 5
-// console.log(addValue)
-// const arr = [10, 20, 30, 40, 50]
-// function add(values) {
-// 	let result = 0
-// 	for (const val of values) {
-// 		result += val
-// 	}
-// 	return result
-// }
+/* function add(val1, val2) {
+  const result = val1 + val2;
+  return result;
+}
+const addValue = add(20, 30) * 5;
+console.log(addValue); */
 
-// const findNumber = (arr, num) {
-// 	let result
-// 	for (const el of arr) {
-// 		if (el === num) {
-// 			result = 'Found'
-// 			break
-// 		} else {
-// 			result = 'Not Found'
-// 		}
-// 	}
-// 	return result
-// }
+/* const arr = [10, 20, 30, 40, 50]
+function add(values) {
+	let result = 0
+	for (const val of values) {
+		result += val
+	}
+	return result
+}
+console.log(result);*/
 
-// const result = findNumber(arr, 30)
-// console.log(`Your query number 30 is ${result}`)
-// ;(function () {
-// 	console.log('Hello Bangladesh')
-// })()
+/**funtion findNumber = (arr, num) {
+	let result
+	for (const el of arr) {
+		if (el === num) {
+			result = 'Found'
+			break
+		} else {
+			result = 'Not Found'
+		}
+	}
+	return result
+}
+const result = findNumber(arr, 30)
+console.log(result); OR
+console.log(`Your query number 30 is ${result}`); **/
 
-//* Different types of parameters
-// function greeting(name = 'Guest') {
-// 	console.log('Hello,' + name)
-// }
-// greeting()
-// function add(a = 0, b = 0) {
-// 	console.log(a + b)
-// }
-// add(30, 30)
+// IIFE: Immediately;
 
-// function sumAll(...numbers) {
-// 	let sum = 0
-// 	for (const num of numbers) {
-// 		sum += num
-// 	}
-// 	console.log(sum)
-// }
-// sumAll(10, 20, 30)
-// sumAll(10, 20)
-// sumAll(10, 20, 30, 50)
+/**(function () {
+  console.log("Hello Bangladesh");
+})(); **/
 
-// const numbers = [40, 10, 200, 50]
-// numbers.sort(function (a, b) {
-// 	console.log(a, b)
-// 	return a - b // 10,  40 50 200,
-// })
-// console.log(numbers)
+//Different type of Parametar:
+//****Defult Parameters Set:****//
+
+/* function greeting(name = "Guest") {
+  console.log("Hello," + name);
+}
+greeting(); */
+
+/* function add(a = 0, b = 0) {
+  console.log(a + b);
+}
+add(30, 30);*/
+
+/*function sumAll(...numbers) {
+  let sum = 0;
+  for (const num of numbers) {
+    sum += num;
+  }
+  console.log(sum);
+}
+sumAll(10, 20, 30);
+sumAll(10, 20);
+sumAll(10, 20, 30, 50);*/
+
+/*const numbers = [40, 10, 200, 50];
+numbers.sort(function (a, b) {
+  console.log(a, b);
+  return a - b; // 10,  40 50 200,
+});
+console.log(numbers);*/
 
 // Problem 01:
-// function cubeNumber(number) {
-// 	if (typeof number !== 'number' || number <= 0) {
-// 		return 'Invalid Input'
-// 	}
-// 	return number * number * number
-// }
+function cubeNumber(number) {
+  if (typeof number !== "number" || number <= 0) {
+    return "Invalid Input";
+  }
+  return number * number * number;
+}
 
-// problem 02
-// function matchFinder(string1, string2) {
-// 	if (string1.includes(string2)) {
-// 		return true
-// 	} else {
-// 		return false
-// 	}
-// }
-// console.log(matchFinder('Jo Doe', 'ohn'))
+// problem 02:
+function matchFinder(string1, string2) {
+  if (typeof string1 !== "string" || typeof string2 !== "string") {
+    return "Please provide valid strings as input";
+  }
+
+  if (string1.includes(string2)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(matchFinder("Jo Doe", "ohn"));
+console.log(matchFinder(123, "ohn"));
+
+//Problem 03:
 function sortMaker(arr) {
-	// if the values one is greater then value 2
-	if (arr[0] < 0 || arr[1] < 0) {
-		return 'Invalid Input'
-	} else if (arr[0] === arr[1]) {
-		return 'Equal'
-	} else if (arr[0] < arr[1]) {
-		arr.sort(function (a, b) {
-			return b - a
-		})
-	} else {
-		return arr
-	}
+  // if the values one is greater then value 2
+  if (arr[0] < 0 || arr[1] < 0) {
+    return "Invalid Input";
+  } else if (arr[0] === arr[1]) {
+    return "Equal";
+  } else if (arr[0] < arr[1]) {
+    arr.sort(function (a, b) {
+      return b - a;
+    });
+  } else {
+    return arr;
+  }
 
-	return arr
+  return arr;
 }
 
-// console.log(sortMaker([3, 2]))
-// console.log(sortMaker([2, 2]))
-// console.log(sortMaker([2, -2]))
+console.log(sortMaker([3, 2]));
+console.log(sortMaker([2, 2]));
+console.log(sortMaker([2, -2]));
 
-// Problem 4
+// Problem 4:
 function findAddress(obj) {
-	// 10,15A, Earth Perfect
-	let streetRes = ''
-	if (obj.street) {
-		streetRes = obj.street
-	} else {
-		streetRes = '__'
-	}
+  // 10,15A, Earth Perfect
+  let streetRes = "";
+  if (obj.street) {
+    streetRes = obj.street;
+  } else {
+    streetRes = "__";
+  }
 
-	let houseRes = ''
-	if (obj.house) {
-		houseRes = obj.house
-	} else {
-		houseRes = '__'
-	}
-	let societyRes = ''
-	if (obj.house) {
-		societyRes = obj.society
-	} else {
-		societyRes = '__'
-	}
-	return `${streetRes},${houseRes}, ${societyRes}`
+  let houseRes = "";
+  if (obj.house) {
+    houseRes = obj.house;
+  } else {
+    houseRes = "__";
+  }
+  let societyRes = "";
+  if (obj.house) {
+    societyRes = obj.society;
+  } else {
+    societyRes = "__";
+  }
+  return `${streetRes},${houseRes}, ${societyRes}`;
 }
-// console.log(findAddress({ street: 10, house: '15A', society: 'Earth Perfect' }))
-// console.log(findAddress({ street: 10, society: 'Earth Perfect' }))
+console.log(
+  findAddress({ street: 10, house: "15A", society: "Earth Perfect" })
+);
+console.log(findAddress({ street: 10, society: "Earth Perfect" }));
 
-// problem 05:
-// function canPay(changeArray, totalDue) {
-// 	if (changeArray.length === 0) {
-// 		return 'No change found'
-// 	}
-// 	let total = 0
-// 	for (const single of changeArray) {
-// 		total += single
-// 	}
+//problem 05:
+function canPay(changeArray, totalDue) {
+  if (changeArray.length === 0) {
+    return "No change found";
+  }
+  let total = 0;
+  for (const single of changeArray) {
+    total += single;
+  }
 
-// 	if (total >= totalDue) {
-// 		return true
-// 	} else {
-// 		return false
-// 	}
-// }
+  if (total >= totalDue) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// console.log(canPay([], 10))
+console.log(canPay([], 10));
--- JavaScript Module/JS-problems-part1/convert.js
+++ JavaScript Module/JS-problems-part1/convert.js
@@ -1,17 +1,19 @@
 // 12 inch 1 feet
-
+// ইঞ্চি থেকে ফিট:
 function inchToFeet(inch) {
-    const feet = inch / 12;
-    return feet;
+  const feet = inch / 12;
+  return feet;
 }
+
+console.log(inchToFeet(24)); // 2 feet
 
 // ignore this one
 function inchToFeet2(inch) {
-    const feetFraction = inch / 12;
-    const feetNumber = parseInt(feetFraction);
-    const inchRemaining = inch % 12;
-    const result = feetNumber + ' ft ' + inchRemaining + ' inch.'
-    return result;
+  const feetFraction = inch / 12;
+  const feetNumber = parseInt(feetFraction);
+  const inchRemaining = inch % 12;
+  const result = feetNumber + " ft " + inchRemaining + " inch.";
+  return result;
 }
 
 const shuvoHeight = inchToFeet(75);
@@ -19,12 +21,19 @@ const shuvoHeight = inchToFeet(75);
 const shuvoHeight2 = inchToFeet2(75);
 // console.log(shuvoHeight2)
 
-function mileToKilometer (mile){
-    const kilo = mile * 1.60934;
-    return kilo;
+// ফিট থেকে ইঞ্চি:
+function feetToInch(feet) {
+  return feet * 12;
 }
 
-function kiloMeterToMiles(kilo){
-    const mile = kilo * 0.621371;
-    return mile;
+console.log(feetToInch(5)); // 60 inches
+
+function mileToKilometer(mile) {
+  const kilo = mile * 1.60934;
+  return kilo;
+}
+
+function kiloMeterToMiles(kilo) {
+  const mile = kilo * 0.621371;
+  return mile;
 }
