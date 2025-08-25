397ad9fd81eb08f72bb2fe2e56ac0dccacf75818
KulsumDiti <kulsumditi3@gmail.com>
2025-08-25 21:23:20 +0600

    conceptual & payoo app added

--- Tour-of-Dom/Payooo-Mobile-Bank-Module-26/home.js
+++ Tour-of-Dom/Payooo-Mobile-Bank-Module-26/home.js
@@ -110,6 +110,7 @@ document.getElementById("send-btn").addEventListener("click", function (e) {
 
 document.getElementById("bonus-btn").addEventListener("click", function (e) {
   e.preventDefault();
+
   const bonusCoupon = document.getElementById("bonus-coupon").value;
   const availableBalance = parseInt(
     document.getElementById("available-balance").innerText
@@ -118,11 +119,51 @@ document.getElementById("bonus-btn").addEventListener("click", function (e) {
     const bonusBalance = parseInt("5000");
     const newAddBalance = availableBalance + bonusBalance;
     document.getElementById("available-balance").innerText = newAddBalance;
+    alert("You receive 5000$");
   } else {
-    alert("Wrong Coupon");
+    alert("Please Enter coupon-code reyal@diti");
   }
 
   fieldClear("bonus-coupon");
+});
+
+// Pay-Bill :
+document.getElementById("pay-bill-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const payBill = document.getElementById("pay-billl").value;
+  const billerNumber = document.getElementById("Bill-number").value;
+
+  const amount = parseInt(document.getElementById("pay-amount").value);
+
+  const pinNumber = parseInt(document.getElementById("add-pin4").value);
+
+  const availableBalance = parseInt(
+    document.getElementById("available-balance").innerText
+  );
+  if (billerNumber.length < 11 || billerNumber.length > 11) {
+    alert("Invalid biller Number");
+    return;
+  }
+  if (amount <= 0 || amount >= availableBalance) {
+    alert("No Balance Available");
+    return;
+  }
+  if (pinNumber !== validPin) {
+    alert("Invalid pin Number");
+    return;
+  }
+  const newAvailableBalance = availableBalance - amount;
+  document.getElementById("available-balance").innerText = newAvailableBalance;
+  alert("Successfully Done");
+  fieldClear("pay-billl");
+  fieldClear("pay-amount");
+  fieldClear("add-pin4");
+  fieldClear("Bill-number");
+});
+//Log-out:
+document.getElementById("log-out-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "./index.html";
 });
 
 // toggling feature
@@ -131,6 +172,9 @@ document.getElementById("add-button").addEventListener("click", function (e) {
   document.getElementById("add-money-parent").style.display = "block";
   document.getElementById("cash-out-parent").style.display = "none";
   document.getElementById("transfer-money-parent").style.display = "none";
+  document.getElementById("pay-bill-parent").style.display = "none";
+  document.getElementById("get-bonus-parent").style.display = "none";
+  document.getElementById("transaction-history-parent").style.display = "none";
   buttonCard("add-button");
 });
 document
@@ -139,6 +183,10 @@ document
     document.getElementById("cash-out-parent").style.display = "block";
     document.getElementById("add-money-parent").style.display = "none";
     document.getElementById("transfer-money-parent").style.display = "none";
+    document.getElementById("pay-bill-parent").style.display = "none";
+    document.getElementById("get-bonus-parent").style.display = "none";
+    document.getElementById("transaction-history-parent").style.display =
+      "none";
     buttonCard("cash-out-button");
   });
 document
@@ -146,6 +194,10 @@ document
   .addEventListener("click", function (e) {
     document.getElementById("add-money-parent").style.display = "none";
     document.getElementById("cash-out-parent").style.display = "none";
+    document.getElementById("pay-bill-parent").style.display = "none";
+    document.getElementById("get-bonus-parent").style.display = "none";
+    document.getElementById("transaction-history-parent").style.display =
+      "none";
     document.getElementById("transfer-money-parent").style.display = "block";
     buttonCard("transfer-button");
   });
@@ -155,12 +207,45 @@ document
     document.getElementById("add-money-parent").style.display = "none";
     document.getElementById("cash-out-parent").style.display = "none";
     document.getElementById("transfer-money-parent").style.display = "none";
+    document.getElementById("pay-bill-parent").style.display = "none";
+    document.getElementById("transaction-history-parent").style.display =
+      "none";
     document.getElementById("get-bonus-parent").style.display = "block";
     buttonCard("get-bonus-button");
   });
-
+document
+  .getElementById("pay-bill-button")
+  .addEventListener("click", function (e) {
+    document.getElementById("add-money-parent").style.display = "none";
+    document.getElementById("cash-out-parent").style.display = "none";
+    document.getElementById("transfer-money-parent").style.display = "none";
+    document.getElementById("get-bonus-parent").style.display = "none";
+    document.getElementById("transaction-history-parent").style.display =
+      "none";
+    document.getElementById("pay-bill-parent").style.display = "block";
+    buttonCard("pay-bill-button");
+  });
+document
+  .getElementById("transactions-button")
+  .addEventListener("click", function (e) {
+    document.getElementById("add-money-parent").style.display = "none";
+    document.getElementById("cash-out-parent").style.display = "none";
+    document.getElementById("transfer-money-parent").style.display = "none";
+    document.getElementById("get-bonus-parent").style.display = "none";
+    document.getElementById("pay-bill-parent").style.display = "none";
+    document.getElementById("transaction-history-parent").style.display =
+      "block";
+    buttonCard("transactions-button");
+  });
 //button-card function:
+
 function buttonCard(id) {
+  const allCards = document.getElementsByClassName("card");
+  for (const card of allCards) {
+    card.style.backgroundColor = "#fff";
+    card.style.borderColor = "#ccc";
+  }
+
   document.getElementById(id).style.backgroundColor = "rgba(8, 116, 242, 0.05)";
   document.getElementById(id).style.borderColor = "#0874F2";
 }
